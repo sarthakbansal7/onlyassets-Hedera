86d16b1f3c92c7dc7d7c11148b5ea157d33ce192
Sarthak <sarthakbansalog@gmail.com>
2025-08-08 22:12:52 +0530

    Creating Interface for Hedera

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_HEDERA_ACCOUNT_ID: string
+  readonly VITE_HEDERA_PRIVATE_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
